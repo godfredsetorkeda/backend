e551aa22485749e33930e9699e60c8bec5f3aa0b
godfredsetorkeda <141179209+godfredsetorkeda@users.noreply.github.com>
2023-08-21 13:47:37 +0000

    Update routes.py

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from .extensions import db
 from .models import User, Data, Plug
-from .sqlalchemy import func
+from sqlalchemy import func
 
 main = Blueprint('main', __name__)
 
